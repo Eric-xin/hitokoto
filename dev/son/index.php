41e788b62f5375c833fdd464c8ddc429445b3b2e
Eric-xin <doreanmon@yahoo.com>
2020-07-17 03:04:04 +0000

    fix a few problem

--- dev/son/index.php
+++ dev/son/index.php
@@ -20,185 +20,47 @@
     <link rel="alternate icon" type="image/png" href="icon.png">
 
     <script src="./script/jquery-3.5.1.min.js"></script>
-    <!--<script src="./script/html2canvas.js"></script>-->
     <script src="https://cdn.bootcss.com/html2canvas/0.5.0-beta3/html2canvas.js"></script>
     <script>
-        function ajax(url) {
-            if (window.XMLHttpRequest) {
-                var oAjax = new XMLHttpRequest();
-            } else
-            {
-                var oAjax = new ActiveXObject("Microsoft.XMLHTTP"); //IE6浏览器创建ajax对象
-            }
-            oAjax.open("GET", url, true); //把要读取的参数的传过来。
-            oAjax.send();
-            oAjax.onreadystatechange = function() {
-                if (oAjax.readyState == 4) {
-                    if (oAjax.status == 200) {
-                        xianshi(oAjax.responseText); //成功的时候调用这个方法
-                    } else
-                    {}
-                }
-            };
+        var word_list=new Array("孙子","儿子","同学","爸爸","爷爷");
+        function change_word(){
+            var word_num=Math.round(Math.random()*4);
+            xianshi(word_list[word_num]);
+            ei(word_num);
         }
+        
+        setInterval("change_word()",200);
+        
         function xianshi(str) {
             $("div#1").html(str); //将读取的内容输出
             console.log(str);
         }
-    </script>
-    <script>
-        function change_sen() {
-            ajax('./api');
+        
+        function ei(word_id){
+            // var id = 2;
+            $("div#2").html('<a class=\"btn btn-primary btn-xs\" onclick=\"click_jump('+word_id+')\");>诶!</a>')
         }
-        var id;
-        function auto_change_sen() {
-            id = setInterval("ajax('./api')", 3000);
+        
+        function click_jump(word_id){
+            url='./result.php?id='+word_id;
+            jump(url);
         }
-        function stop_auto_change_sen() {
-            clearInterval(id);
+        
+        function jump(url){
+            window.location.href=url;
         }
-
-        var tmp = 0;
-        function auto_change() {
-            tmp++;
-            if (tmp%2 == 0) {
-                stop_auto_change_sen();
-            } else {
-                auto_change_sen();
-            }
-        }
+        // setInterval("ajax('https://v1.hitokoto.cn/?d=f&encode=text')", 9000);
+        // setInterval("ajax('http://ericxin.cf:234/beta/hitokoto-master/api')", 3000);
+        // setInterval("ajax('./api')", 3000);
+        // setInterval("ajax('./api')");
     </script>
 
     <script>
-        function nyan_cat() {
-            var include = document.createElement('iframe');
-            include.style.position = 'fixed';
-            include.style.left = '0';
-            include.style.top = '0';
-            include.style.width = '100%';
-            include.style.height = '100%';
-            include.frameBorder = '0';
-
-            // make sure that iframe is the first element in body
-            if (document.body.hasChildNodes()) {
-                document.body.insertBefore(include, document.body.firstChild);
-            } else {
-                document.body.appendChild(include);
-            }
-
-            // listen to the hash url change to load the page
-            function loadUrl() {
-                if (window.location.hash === '') {
-                    include.src = document.referrer;
-                } else {
-                    include.src = window.location.hash.substr(1);
-                }
-            }
-            loadUrl();
-            window.addEventListener('hashchange', loadUrl, false);
-
-            // return a int random num
-            function getRandomNum(min, max) {
-                var range = max - min;
-                var rand = Math.random();
-                return(min + Math.round(rand * range));
-            }
-
-            // cat class
-            function Cat() {
-
-                // reset position xy and speed
-                this.reset = function() {
-                    this.img.width = getRandomNum(100, 200);
-                    this.x = -this.img.width;
-                    this.y = getRandomNum(0, window.innerHeight - 100);
-                    this.img.style.left = this.x + 'px';
-                    this.img.style.top = this.y + 'px';
-                    this.speed = getRandomNum(1, 5);
-                };
-
-                this.img = document.createElement('img');
-                this.img.src = 'nyancat/img/nyancat.gif';
-                this.img.style.position = 'fixed';
-                this.wait = true; // ture is not display
-                document.body.appendChild(this.img);
-                this.reset();
-
-                // this should call in loop update callback
-                this.update = function(dt) {
-                    if (this.wait) {
-                        if (getRandomNum(0, 180) === 0) {
-                            // about 3 seconds
-                            this.wait = false;
-                            this.reset();
-                        }
-                    } else {
-                        this.x += this.speed;
-                        this.img.style.left = this.x + 'px';
-                        if (this.x > window.innerWidth + this.img.width) {
-                            this.wait = true;
-                        }
-                    }
-                };
-
-            }
-
-            // cat array used to manage
-            var cats = [];
-
-            // load callback
-            function load() {
-                // init cats
-                for (var n = 0; n < 20; n++) {
-                    cats[n] = new Cat();
-                }
-                // play bgm
-                var bgm = document.createElement('audio');
-                bgm.autoplay = true;
-                bgm.loop = true;
-                var src1 = document.createElement('source');
-                src1.src = 'nyancat/bgm/nyancat.mp3';
-                src1.type = 'audio/mpeg';
-                bgm.appendChild(src1);
-                var src2 = document.createElement('source');
-                src2.src = 'nyancat/bgm/nyancat.ogg';
-                src2.type = 'audio/ogg';
-                bgm.appendChild(src2);
-                document.body.appendChild(bgm);
-            }
-
-            // update callback
-            function update(dt) {
-                cats.forEach(function (cat) {
-                    cat.update(dt);
-                });
-            }
-
-            // start loop engine
-            function start() {
-                // make a fps loop frame
-                var fps = 60;
-                var lastTime = new Date().getTime();
-                var loop = function() {
-                    var nowTime = new Date().getTime();
-                    var deltaTime = nowTime - lastTime;
-                    if (deltaTime - 1000 / fps >= 0) {
-                        lastTime = nowTime;
-                        update(deltaTime / 1000);
-                    }
-                };
-                // load callback
-                load();
-                // start loop as soon as possible
-                window.setInterval(loop, 1);
-            }
-            start();
-        }
-        ajax('./api');
         console.log("没想到你打开了F12!\n这是由ericxin开发的真鸡汤系统\n提供api ./api\n可以使试一下毒鸡汤 ericxin.cf:234");
         // confirm("欢迎来到真鸡汤");
     </script>
     <script>
+    
         function get_image() {
             var userAgent = navigator.userAgent;
             var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1;
@@ -259,7 +121,7 @@
                     </div>
                     <div class="col">
                         <div class="float-right" style="padding-top: 0px;">
-                            <a class="btn btn-primary btn-filled btn-xs" onclick="auto_change();">自动</a>
+                            <!--<a class="btn btn-primary btn-filled btn-xs" onclick="auto_change();">自动</a>-->
                             <!--<a class="btn btn-primary btn-filled btn-xs" onclick="stop_acs();">停止</a>-->
                         </div>
                     </div>
@@ -269,11 +131,14 @@
 
         <div class="main-wrapper" style="position: relative; top: -6vh;">
             <div class="container main-sentence justify-content-center text-center">
+                <!--<div id="1"><span id="sentence" style="font-size: 2rem;"></span></div>-->
                 <span id="sentence" style="font-size: 2rem;"><div id="1"></div>
                 </span>
                 <!--<br>-->
-                <a class="btn btn-primary btn-xs" onclick="change_sen();">换一句</a>
-                <a class="btn btn-primary btn-xs" onclick="nyan_cat();">鬼畜</a>
+                <!--<a class="btn btn-primary btn-xs" onclick="change_sen();">换一句</a>-->
+                <!--<a class="btn btn-primary btn-xs" onclick="nyan_cat();">鬼畜</a>-->
+                <!--<a class="诶!" onclick="nyan_cat();">鬼畜</a>-->
+                <div id="2"></div>
             </a>
         </div>
     </div>
@@ -291,7 +156,7 @@
             </div>
         </div>
     </div>
-    <div class="foot-1" style="position: absolute; bottom: 0vh;width: 0%;">
+    <div class="foot-1" style="position: absolute; bottom: 10vh;width: 100%;">
     <div class="container">
         <meting-js
             mini="true"
